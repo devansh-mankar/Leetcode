a2673c2774ee0d5efd345fa2a5de9330efb17a51
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-05-07 15:39:43 +0530

    Time: 12 ms (89.30%) | Memory: 26.8 MB (48.72%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
+        //moore's voting algorithm
 
-        map<int,int>mp;
+        //keeping track of minority and majority elements
+        int n=nums.size();
+        int cnt=0,ele=0;
 
         for(int i=0;i<n;i++)
         {
-            mp[nums[i]]++;
+            if(cnt==0)
+            {
+                ele=nums[i];
+            }
+            if(ele==nums[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+            
+
         }
 
-        for(auto it:mp)
-        {
-            if(it.second>n/2)
-            {
-                return it.first;
-            }
-        }
-        return -1;
+       return ele;
     }
 
 };
