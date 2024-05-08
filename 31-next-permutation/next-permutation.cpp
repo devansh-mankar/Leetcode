21efda0412024a18972880740abdaa9f26418edc
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-05-08 12:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (15.28%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+
+        int index=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i+1]>nums[i])
+            {
+                //get the dip
+                index=i;
+                break;
+            }
+        }
+        if(index==-1)
+        {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        for(int i=n-1;i>=0;i--)
+        {
+            if(nums[i]>nums[index])
+            {
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
