b28a8af61a7350fe9c4c7f5ef372278ff2b665b8
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-05-24 11:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (6.01%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,22 +10,18 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* head)
+    {
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        ListNode* newHead=reverse(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return newHead;
+    }
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp=head;
-        stack<int>st;
-
-        while(temp!=NULL)
-        {
-            st.push(temp->val);
-            temp=temp->next;
-        }
-        temp=head;
-        while(temp!=NULL)
-        {
-            temp->val=st.top();
-            st.pop();
-            temp=temp->next;
-        }
-        return head;
+      return  reverse(head);
     }
 };
