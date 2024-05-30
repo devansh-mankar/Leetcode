e3019d2aae468ee0ad4eae88b186dd56ff33b54f
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-05-30 14:55:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (67.51%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
+    long solve(string &s,int sign,int i,long result)
+    {
+        if((sign*result)>=INT_MAX)
+        {
+            return INT_MAX;
+        }
+        if((sign*result)<=INT_MIN)
+        {
+            return INT_MIN;
+        }
+        if(i>=s.length() || s[i]<'0' || s[i]>'9')
+        {
+            return sign*result;
+        }
+        return solve(s,sign,i+1,(result*10+s[i]-'0'));
+    
+    }
     int myAtoi(string s) {
-       
-        long result=0;
-        int sign=1;
+        int n=s.size();
         int i=0;
-            while(i<s.length() && s[i]==' ')
-            {
-                i++;
-            }
-            if(s[i]=='-')
-            {
-                sign=-1;
-                i++;
-            }
-            else if(s[i]=='+')
-            {
-                i++;
-            }
-           while(i<s.length())
-           {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                result=result*10+(s[i]-'0');
-                if(result>INT_MAX && sign==-1)
-                {
-                    return INT_MIN;
-                }
-                else if(result>INT_MAX && sign==1)
-                {
-                    return INT_MAX;
-                }
-                i++;
-            }
-            else
-            {
-                return result*sign;
-            }
-           }
+        int sign=1;
+
+       
         
-        return (result*sign);
+        while(i<s.length() && s[i]==' ')
+        {
+            i++;
+        }
+        if(s[i]=='-')
+        {
+            sign=-1;
+            i++;
+        }
+        else if(s[i]=='+')
+        {
+            i++;
+        }
+        return solve(s,sign,i,0);
+        
     }
 };
