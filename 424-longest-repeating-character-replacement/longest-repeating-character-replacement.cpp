f02c0f943368112954c13621601acc9ba56fcb11
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-06-04 11:10:43 +0530

    Time: 3 ms (96.64%) | Memory: 8.2 MB (58.47%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -4,24 +4,18 @@ public:
         int n=s.size();
         int left=0,right=0;
         int maxlen=0;
-        vector<int>hash(26,0);
         int maxFreq=0;
+        vector<int>hash(26,0);
+
         while(right<n)
         {
             hash[s[right]-'A']++;
             maxFreq=max(maxFreq,hash[s[right]-'A']);
-          
 
-            while(((right-left+1)-maxFreq)>k)
+            if(((right-left+1)-maxFreq)>k)
             {
                 hash[s[left]-'A']--;
-                maxFreq=0;
                 left++;
-                for(int i=0;i<26;i++)
-                {
-                    maxFreq=max(maxFreq,hash[i]);
-                }
-                
             }
             if(((right-left+1)-maxFreq)<=k)
             {
