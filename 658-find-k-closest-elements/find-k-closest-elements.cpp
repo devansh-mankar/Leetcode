b05468409279475c41f290757e4bd6dc6d9e348c
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-06-09 13:02:03 +0530

    Time: 32 ms (50.21%) | Memory: 34.8 MB (46.20%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,22 +1,62 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int>ans;
-        priority_queue<pair<int,int>>pq;
-
-        for(int i=0;i<arr.size();i++)
+    int BinarySearch(vector<int>&arr,int n,int x)
+    {
+        if(arr[0]>x)
         {
-            pq.push({abs(arr[i]-x),arr[i]});
-            if(pq.size()>k)
+            return 0;
+        }
+        int s=0,e=n-1;
+
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+
+            if(arr[mid]==x)
             {
-                pq.pop();
+                return mid;
+            }
+            if(arr[mid]<x)
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
             }
         }
+        return s;
+    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int>ans;
+        int n=arr.size();
+        int pos=BinarySearch(arr,n,x);
+        int i=pos-1,j=pos;
 
-        while(!pq.empty())
+        while(k>0 && i>=0 && j<n)
         {
-            ans.push_back(pq.top().second);
-            pq.pop();
+            if(abs(x-arr[i])<=abs(arr[j]-x))
+            {
+                ans.push_back(arr[i--]);
+            }
+            else
+            {
+                ans.push_back(arr[j++]);
+            }
+            k--;
+        } 
+
+        //for edge cases
+        while(k--)
+        {
+            if(i<0)
+            {
+                ans.push_back(arr[j++]);
+            }
+            else if(j>=n)
+            {
+                ans.push_back(arr[i--]);
+            }
         }
         sort(ans.begin(),ans.end());
         return ans;
