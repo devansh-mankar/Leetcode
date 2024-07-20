c604757b8cb08c3fcd11b29d4d445b71e7e2c2a4
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-07-20 15:36:25 +0530

    Time: 105 ms (77.58%) | Memory: 98 MB (50.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,31 +1,8 @@
 class Solution {
 public:
-    bool solve(int index,int k,vector<int>&nums,vector<vector<int>>&dp)
-    {
-        if(k==0)
-        {
-            return true;
-        }
-        if(index==0)
-        {
-            return nums[0]==k;
-        }
-        if(dp[index][k]!=-1)
-        {
-            return dp[index][k];
-        }
-        bool notpick=solve(index-1,k,nums,dp);
-        bool pick=false;
-        if(nums[index]<=k)
-        {
-            pick=solve(index-1,k-nums[index],nums,dp);
-        }
-        return dp[index][k]=pick || notpick;
-    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int k=accumulate(nums.begin(),nums.end(),0);
-
         if(k%2==1)
         {
             return false;
@@ -33,8 +10,30 @@ public:
         else
         {
             int k1=k/2;
-            vector<vector<int>>dp(n,vector<int>(k1+1,-1));
-            return solve(n-1,k1,nums,dp);
+            vector<int>prev(k1+1,0);
+            prev[0]=true;
+            if(nums[0]<=k1)
+            {
+                prev[nums[0]]=true;
+            }
+
+            for(int i=1;i<n;i++)
+            {
+                vector<int>curr(k1+1,0);
+                curr[0]=0;
+                for(int j=0;j<=k1;j++)
+                {
+                    bool notpick=prev[j];
+                    bool pick=false;
+                    if(nums[i]<=j)
+                    {
+                        pick=prev[j-nums[i]];
+                    }
+                    curr[j]=pick || notpick;
+                }
+                prev=curr;
+            }
+            return prev[k1];
         }
     }
 };
