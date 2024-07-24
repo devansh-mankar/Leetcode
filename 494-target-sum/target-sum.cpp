72a6e9a473429070d85c9771c6c17fc603740c22
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2024-07-24 14:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (41.65%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-    int solve(int index,vector<int>&nums,int k,vector<vector<int>>&dp)
-    {
-        if(index==0)
-        {
-            if(k==0 && nums[0]==0)
-            {
-                return 2;
-            }
-            if(k==0 || nums[0]==k)
-            {
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[index][k]!=-1)
-        {
-            return dp[index][k];
-        }
-        int notpick=solve(index-1,nums,k,dp);
-        int pick=0;
-        if(nums[index]<=k)
-        {
-            pick=solve(index-1,nums,k-nums[index],dp);
-        }
-        return dp[index][k]=(pick+notpick);
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int total=accumulate(nums.begin(),nums.end(),0);
@@ -33,12 +7,43 @@ public:
         {
             return 0;
         }
+
         if((total-target)%2==1)
         {
             return 0;
         }
         int k=(total-target)/2;
-        vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return solve(n-1,nums,k,dp);
+        vector<vector<int>>dp(n,vector<int>(k+1,0));
+        if(nums[0]==0)
+        {
+            dp[0][0]=2;
+        }
+        else
+        {
+            dp[0][0]=1;
+        }
+
+        if(nums[0]!=0 && nums[0]<=k)
+        {
+            dp[0][nums[0]]=1;
+        }
+
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=k;j++)
+            {
+                int notpick=dp[i-1][j];
+                int pick=0;
+                if(nums[i]<=j)
+                {
+                    pick=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=(pick+notpick);
+            }
+        }
+
+        return dp[n-1][k];
+
     }
 };
