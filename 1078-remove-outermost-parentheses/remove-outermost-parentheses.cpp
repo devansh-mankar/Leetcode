3875b959289e9964f1a6235ad249e1a152290d35
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-06-23 19:34:58 +0530

    Time: 1 ms (29.06%) | Memory: 9 MB (53.63%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans="";
         int depth=0;
+        string ans="";
 
         for(auto it:s)
         {
             if(it=='(')
             {
+                
                 depth++;
             }
             if(depth!=1)
-            {
-                ans.push_back(it);
-            }
-             if(it==')')
-            {
+                {
+                    ans+=it;
+                }
+            if(it==')'){
                 depth--;
             }
         }
