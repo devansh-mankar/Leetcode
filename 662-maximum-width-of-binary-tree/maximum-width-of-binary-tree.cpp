0212364baf81341e221ea2e3b6d6178c0d3713ca
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-10 13:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (35.84%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -16,41 +16,40 @@ public:
         {
             return 0;
         }
-       long long int ans=0;
-        queue<pair<TreeNode*,long long int>>q;
+        long long ans=0;
+        queue<pair<TreeNode*,int>>q;
         q.push({root,0});
 
         while(!q.empty())
         {
-           long long int n=q.size();
-           long long int leftmost,rightmost;
-           long long  int currMin=q.front().second;
+            int n=q.size();
+            long long leftMost,rightMost;
+            long long currMin=q.front().second;
 
             for(int i=0;i<n;i++)
             {
-                TreeNode* temp=q.front().first;
-               long long int currIndex=q.front().second-currMin;
+                auto node=q.front().first;
+                long long currIndex=q.front().second-currMin;
                 q.pop();
-                
+
                 if(i==0)
                 {
-                    leftmost=currIndex;
+                    leftMost=currIndex;
                 }
                 if(i==n-1)
                 {
-                    rightmost=currIndex;
+                    rightMost=currIndex;
                 }
-
-                if(temp->left)
+                if(node->left)
                 {
-                    q.push({temp->left,currIndex*2+1});
+                    q.push({node->left,currIndex*2+1});
                 }
-                if(temp->right)
+                if(node->right)
                 {
-                    q.push({temp->right,currIndex*2+2});
+                    q.push({node->right,currIndex*2+2});
                 }
             }
-            ans=max(ans,(rightmost-leftmost+1));
+            ans=max(ans,rightMost-leftMost+1);
         }
         return ans;
     }
