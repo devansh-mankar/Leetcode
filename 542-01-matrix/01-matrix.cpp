f279a60da4f6e88ef78a197f9cc67860e4cb787d
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-12 22:38:22 +0530

    Time: 11 ms (87.33%) | Memory: 34.7 MB (87.37%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,51 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
-
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>ans(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
+        queue<pair<int,int>>q;
+        vector<vector<int>>dis(n,vector<int>(m,-1));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    q.push({{i,j},0});
-                    vis[i][j]=1;
-                    ans[i][j]=0;
+                    q.push({i,j});
+                    dis[i][j]=0;
                 }
             }
         }
 
-
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-
+        vector<int>dr={-1,0,1,0};
+        vector<int>dc={0,1,0,-1};
         while(!q.empty())
         {
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int steps=q.front().second;
-            ans[row][col]=steps;
-
+            int row=q.front().first;
+            int col=q.front().second;
             q.pop();
 
             for(int i=0;i<4;i++)
             {
-                int nrow=row+dr[i];
-                int ncol=col+dc[i];
-
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol])
-                {
-                    q.push({{nrow,ncol},steps+1});
-                    vis[nrow][ncol]=true;
-                }
+                int nr=row+dr[i];
+                int nc=col+dc[i];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && dis[nr][nc]==-1)
+            {
+                q.push({nr,nc});
+                dis[nr][nc]=dis[row][col]+1;
+            }
             }
         }
-        return ans;
+        return dis;
     }
 };
