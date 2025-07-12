6ee046893feaadd949b4ee1280095c1ff3048822
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-12 20:32:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (90.52%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void solve(int row,int col,int color,int original,vector<vector<int>>&image)
+    void dfs(int row,int col,int color,int original,vector<vector<int>>&image)
     {
-        if(row<0 || row>=image.size() || col<0 || col>=image[0].size() || image[row][col]==color || image[row][col]!=original)
+        if(row<0 || row>=image.size() || col<0 || col>=image[0].size() || image[row][col]!=original || image[row][col]==color)
         {
-            return ;
+            return;
         }
         image[row][col]=color;
-        solve(row-1,col,color,original,image);
-        solve(row,col+1,color,original,image);
-        solve(row+1,col,color,original,image);
-        solve(row,col-1,color,original,image);
+        dfs(row-1,col,color,original,image);
+        dfs(row,col+1,color,original,image);
+        dfs(row+1,col,color,original,image);
+        dfs(row,col-1,color,original,image);
+
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-       
-      int original=image[sr][sc];
-        solve(sr,sc,color,original,image);
+        int original=image[sr][sc];
+        dfs(sr,sc,color,original,image);
         return image;
     }
 };
