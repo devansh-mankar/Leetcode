95825109d0edca539cdfafe30de092287d887a25
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-13 00:17:33 +0530

    Time: 0 ms (100.00%) | Memory: 33.9 MB (58.15%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
+    void dfs(int row,int col,vector<vector<int>>&grid)
+    {
+        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || grid[row][col]!=1)
+        {
+            return;
+        }
+        grid[row][col]=2;
+        dfs(row-1,col,grid);
+        dfs(row,col+1,grid);
+        dfs(row+1,col,grid);
+        dfs(row,col-1,grid);
+    }
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-
-        queue<pair<int,int>>q;
 
         for(int i=0;i<n;i++)
         {
@@ -14,33 +24,9 @@ public:
                 {
                     if(grid[i][j]==1)
                     {
-                        grid[i][j]=2;
-                    q.push({i,j});
+                        dfs(i,j,grid);
                     }
-                    
                 }
-            }
-        }
-
-        vector<int>dr={-1,0,1,0};
-        vector<int>dc={0,1,0,-1};
-
-        while(!q.empty())
-        {
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-
-            for(int i=0;i<4;i++)
-            {
-                int nr=row+dr[i];
-                int nc=col+dc[i];
-            
-            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1)
-            {
-                q.push({nr,nc});
-                grid[nr][nc]=2;
-            }
             }
         }
 
