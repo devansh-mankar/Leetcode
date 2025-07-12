e90c7cc7ffffdee53424f501f912b315460ada67
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-13 00:08:22 +0530

    Time: 1 ms (97.59%) | Memory: 28.8 MB (96.69%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<int>>&grid)
-    {
-        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || vis[row][col] || grid[row][col]!=1)
-        {
-            return;
-        }
-        vis[row][col]=true;
-        dfs(row+1,col,vis,grid);
-        dfs(row-1,col,vis,grid);
-        dfs(row,col-1,vis,grid);
-        dfs(row,col+1,vis,grid);
-    }
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
 
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
 
         for(int i=0;i<n;i++)
         {
@@ -26,17 +14,42 @@ public:
                 {
                     if(grid[i][j]==1)
                     {
-                        dfs(i,j,vis,grid);
+                        grid[i][j]=2;
+                    q.push({i,j});
                     }
+                    
                 }
             }
         }
+
+        vector<int>dr={-1,0,1,0};
+        vector<int>dc={0,1,0,-1};
+
+        while(!q.empty())
+        {
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+
+            for(int i=0;i<4;i++)
+            {
+                int nr=row+dr[i];
+                int nc=col+dc[i];
+            
+            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1)
+            {
+                q.push({nr,nc});
+                grid[nr][nc]=2;
+            }
+            }
+        }
+
         int count=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] && grid[i][j]==1)
+                if(grid[i][j]==1)
                 {
                     count++;
                 }
