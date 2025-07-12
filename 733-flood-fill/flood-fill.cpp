0c34dedbf08f4fd878a80ec9cb2ab09433928369
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-12 20:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (90.52%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>>temp=image;
-        int n=image.size();
-        int m=image[0].size();
-
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        temp[sr][sc]=color;
-
-        vector<int>dr={-1,0,1,0};
-        vector<int>dc={0,1,0,-1};
-
-        while(!q.empty())
+    void solve(int row,int col,int color,int original,vector<vector<int>>&image)
+    {
+        if(row<0 || row>=image.size() || col<0 || col>=image[0].size() || image[row][col]==color || image[row][col]!=original)
         {
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-
-            for(int i=0;i<4;i++)
-            {
-                int nr=row+dr[i];
-                int nc=col+dc[i];
-
-                if(nr>=0 && nr<n && nc>=0 && nc<m && temp[nr][nc]!=color && image[sr][sc]==temp[nr][nc])
-                {
-                    q.push({nr,nc});
-                    temp[nr][nc]=color;
-                }
-            }
-
+            return ;
         }
-        return temp;
+        image[row][col]=color;
+        solve(row-1,col,color,original,image);
+        solve(row,col+1,color,original,image);
+        solve(row+1,col,color,original,image);
+        solve(row,col-1,color,original,image);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+       
+      int original=image[sr][sc];
+        solve(sr,sc,color,original,image);
+        return image;
     }
 };
