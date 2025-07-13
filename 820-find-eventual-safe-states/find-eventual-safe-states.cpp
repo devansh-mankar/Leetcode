d7035df1c98ddb78380ebd87fe74a7584301a779
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-14 00:27:08 +0530

    Time: 68 ms (9.98%) | Memory: 66.3 MB (21.23%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int>&vis,vector<int>&dfsCall,vector<vector<int>>&graph,vector<int>&check)
-    {
-        dfsCall[node]=true;
-        vis[node]=true;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //already graph is the adjacency list
+        int n=graph.size();
+        vector<int>indegree(n,0);
+        vector<vector<int>>adj(n);
 
-        for(auto it:graph[node])
+        for(int i=0;i<n;i++)
         {
-            if(!vis[it])
+            for(auto j:graph[i])
             {
-                if(dfs(it,vis,dfsCall,graph,check))
+                adj[j].push_back(i);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        int count=0;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
                 {
-                    return true;
+                    q.push(it);
                 }
             }
-            else if(dfsCall[it])
-            {
-                return true;
-            }
         }
-        check[node]=true;
-        dfsCall[node]=false;
-        return false;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-
-        vector<int>ans;
-        vector<int>vis(n,0);
-        vector<int>dfsCall(n,0);
-        vector<int>check(n,0);
-
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                dfs(i,vis,dfsCall,graph,check);
-            }
-        }
-
-        for(int i=0;i<n;i++)
-        {
-            if(check[i]==1)
-            {
-                ans.push_back(i);
-            }
-        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
