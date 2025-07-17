1492da1ddc2058be3a59be8fcde527fd22812dbe
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-17 20:04:15 +0530

    Time: 13 ms (67.40%) | Memory: 52.6 MB (78.35%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,54 +1,43 @@
 class Solution {
 public:
+    bool dfs(int node,vector<int>&vis,vector<int>&dfsCall,vector<int>&ans,vector<vector<int>>&graph)
+    {
+        vis[node]=true;
+        dfsCall[node]=true;
+        for(auto it:graph[node])
+        {
+            if(!vis[it])
+            {
+                if(dfs(it,vis,dfsCall,ans,graph))
+                {
+                    return true;
+                }
+                
+            }
+            if(dfsCall[it])
+            {
+                return true;
+            }
+
+        }
+        ans.push_back(node);
+        dfsCall[node]=false;
+        return false;
+    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<vector<int>>adj(n);
-
-        for(int i=0;i<n;i++)
-        {
-            for(auto it:graph[i])
-            {
-                adj[it].push_back(i);
-            }
-        }
-
-
-        vector<int>indegree(n,0);
-
-        for(int i=0;i<n;i++)
-        {
-            for(auto it:adj[i])
-            {
-                indegree[it]++;
-            }
-        }
-
-        queue<int>q;
-        for(int i=0;i<n;i++)
-        {
-            if(indegree[i]==0)
-            {
-                q.push(i);
-            }
-        }
+        vector<int>vis(n,0);
+        vector<int>dfsCall(n,0);
         vector<int>ans;
-        while(!q.empty())
+
+        for(int i=0;i<n;i++)
         {
-            auto node=q.front();
-            q.pop();
-            ans.push_back(node);
-            for(auto it:adj[node])
+            if(!vis[i])
             {
-                indegree[it]--;
-                if(indegree[it]==0)
-                {
-                    q.push(it);
-                }
+                dfs(i,vis,dfsCall,ans,graph);
             }
         }
-
         sort(ans.begin(),ans.end());
         return ans;
-
     }
 };
