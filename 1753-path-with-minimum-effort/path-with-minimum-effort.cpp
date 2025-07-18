91581b9f44adc36a15fbc02b5ab2bdec9f55a6f2
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-18 23:57:15 +0530

    Time: 686 ms (5.00%) | Memory: 144.2 MB (5.00%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,45 +1,52 @@
 class Solution {
 public:
-   int minimumEffortPath(vector<vector<int>>& heights) {
-    int left = 0, right = 1000000;
-    
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-        
-        if (canReach(heights, mid)) {
-            right = mid;
-        } else {
-            left = mid + 1;
+    bool dfs(int row,int col,int effort,vector<vector<int>>&heights,vector<vector<int>>&vis)
+    {
+        if(row==heights.size()-1 && col==heights[0].size()-1)
+        {
+            return true;
         }
-    }
-    return left;
-}
+        vis[row][col]=true;
+        vector<int>dr={-1,0,1,0};
+        vector<int>dc={0,1,0,-1};
 
-bool canReach(vector<vector<int>>& heights, int maxEffort) {
-    int m = heights.size(), n = heights[0].size();
-    vector<vector<bool>> visited(m, vector<bool>(n, false));
-    return dfs(heights, 0, 0, maxEffort, visited);
-}
-
-bool dfs(vector<vector<int>>& heights, int row, int col, 
-         int maxEffort, vector<vector<bool>>& visited) {
-    if (row == heights.size()-1 && col == heights[0].size()-1) return true;
-    
-    visited[row][col] = true;
-    int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
-    
-    for (int i = 0; i < 4; i++) {
-        int nr = row + dr[i], nc = col + dc[i];
-        
-        if (nr >= 0 && nr < heights.size() && nc >= 0 && nc < heights[0].size() 
-            && !visited[nr][nc]) {
-            
-            int effort = abs(heights[nr][nc] - heights[row][col]);
-            if (effort <= maxEffort && dfs(heights, nr, nc, maxEffort, visited)) {
-                return true;
+        for(int i=0;i<4;i++)
+        {
+            int nr=row+dr[i];
+            int nc=col+dc[i];
+            if(nr>=0 && nr<heights.size() && nc>=0 && nc<heights[0].size() && !vis[nr][nc])
+            {
+                int neff=abs(heights[nr][nc]-heights[row][col]);
+                if(neff<=effort && dfs(nr,nc,effort,heights,vis))
+                {
+                    return true;
+                }
             }
         }
+        return false;
     }
-    return false;
-}
+    bool canReach(int effort,vector<vector<int>>&heights)
+    {
+        int n=heights.size();
+        int m=heights[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        return dfs(0,0,effort,heights,vis);
+    }
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int left=0,right=1000000;
+
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+
+            if(canReach(mid,heights))
+            {
+                right=mid;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return left;
+    }
 };
