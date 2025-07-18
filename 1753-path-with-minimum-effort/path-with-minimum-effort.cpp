a589ea61919ae2fa4edac545fbb1fd255e4841ad
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-18 22:56:56 +0530

    Time: 50 ms (67.90%) | Memory: 24.4 MB (90.74%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -4,37 +4,34 @@ public:
         int n=heights.size();
         int m=heights[0].size();
 
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
 
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
         pq.push({0,{0,0}});
-        dist[0][0]=0;
 
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
+        vector<int>dr={-1,0,1,0};
+        vector<int>dc={0,1,0,-1};
+        dist[0][0]=0;
 
         while(!pq.empty())
         {
-            int dis=pq.top().first;
+            int diff=pq.top().first;
             int row=pq.top().second.first;
             int col=pq.top().second.second;
             pq.pop();
 
             if(row==n-1 && col==m-1)
             {
-                return dis;
+                return diff;
             }
-
 
             for(int i=0;i<4;i++)
             {
                 int nr=row+dr[i];
                 int nc=col+dc[i];
-
-            if(nr>=0 && nr<n && nc>=0 && nc<m)
+                if(nr>=0 && nr<n && nc>=0 && nc<m)
                 {
-                    int neff=max(abs(heights[row][col]-heights[nr][nc]),dis);
+                    int neff=max(diff,abs(heights[nr][nc]-heights[row][col]));
                     if(neff<dist[nr][nc])
                     {
                         dist[nr][nc]=neff;
@@ -43,6 +40,6 @@ public:
                 }
             }
         }
-        return 0;
+        return -1;
     }
 };
