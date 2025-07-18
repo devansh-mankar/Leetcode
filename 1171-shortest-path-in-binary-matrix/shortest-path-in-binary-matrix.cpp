a473ca7b0ee0ba9737704927f56de7a41277d116
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-18 20:52:34 +0530

    Time: 12 ms (86.76%) | Memory: 23.4 MB (96.85%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,43 +2,36 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-
         if(grid[0][0]==1)
         {
             return -1;
         }
-        
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        queue<pair<int,int>>q;
+        q.push({0,0});
 
-        pq.push({1,{0,0}});
-        dist[0][0]=1;
+        grid[0][0]=1;
         vector<int>dr={-1,0,1,0,1,1,-1,-1};
-        vector<int>dc={0,1,0,-1,1,-1,-1,1};
-        while(!pq.empty())
+        vector<int>dc={0,1,0,-1,1,-1,1,-1};
+        while(!q.empty())
         {
-            int d=pq.top().first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
-            pq.pop();
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
 
+            int currd=grid[row][col];
             if(row==n-1 && col==n-1)
             {
-                return d;
+                return currd;
             }
             for(int i=0;i<8;i++)
             {
                 int nr=row+dr[i];
                 int nc=col+dc[i];
+
                 if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==0)
                 {
-                    int newd=dist[row][col]+1;
-                    if(dist[nr][nc]>newd)
-                    {
-                        dist[nr][nc]=newd;
-                        pq.push({newd,{nr,nc}});
-                    }
-                    
+                    grid[nr][nc]=currd+1;
+                    q.push({nr,nc});
                 }
             }
         }
