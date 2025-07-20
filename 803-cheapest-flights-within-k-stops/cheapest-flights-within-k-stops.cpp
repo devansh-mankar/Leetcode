a6ac13cf8f44c7f76bd3ab3f9ca814f67607f435
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-07-21 00:55:37 +0530

    Time: 160 ms (5.02%) | Memory: 54.1 MB (5.02%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,52 +1,26 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>>adj(n);
+        vector<int>dist(n,INT_MAX);
+        dist[src]=0;
 
-        for(auto it:flights)
+        for(int i=0;i<=k;i++)
         {
-            int u=it[0];
-            int v=it[1];
-            int wt=it[2];
-            adj[u].push_back({v,wt});
-        }
-
-        vector<vector<int>>dist(n,vector<int>(k+2,INT_MAX));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        pq.push({0,{src,0}});
-        dist[src][0]=0;
-
-        while(!pq.empty())
-        {
-            int distance=pq.top().first;
-            int stops=pq.top().second.second;
-            int node=pq.top().second.first;
-            pq.pop();
-
-             if(node==dst)
+            vector<int>temp=dist;
+            for(auto it:flights)
             {
-                return distance;
-            }
+                int u=it[0];
+                int v=it[1];
+                int wt=it[2];
 
-            if(stops>k)
-            {
-                continue;
-            }
-            if(distance>dist[node][stops])
-            {
-                continue;
-            }
-           
-
-            for(auto it:adj[node])
-            {
-                if(stops+1<=k+1 && it.second+distance<dist[it.first][stops+1])
+                if(dist[u]!=INT_MAX && dist[u]+wt<temp[v])
                 {
-                    dist[it.first][stops+1]=it.second+distance;
-                    pq.push({dist[it.first][stops+1],{it.first,stops+1}});
+                    temp[v]=dist[u]+wt;
                 }
             }
+            dist=temp;
         }
-        return -1;
+
+        return dist[dst]==INT_MAX ? -1:dist[dst];
     }
 };
