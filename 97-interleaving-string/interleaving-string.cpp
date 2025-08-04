2ede1fcc5b597419947a5a9c32518637da80add4
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-08-04 14:52:38 +0530

    Time: 6 ms (35.33%) | Memory: 9.7 MB (50.73%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    bool solve(string& s1,string& s2,string& s3,int i,int j,int k,vector<vector<int>>&dp)
-    {
-        if(i==s1.size() && j==s2.size() && k==s3.size())
-        {
-            return true;
-        }
-        if(i>s1.size() || j>s2.size())
-        {
-            return false;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(s3[k]==s1[i] && s3[k]==s2[j])
-        {
-            return dp[i][j]=solve(s1,s2,s3,i+1,j,k+1,dp) || solve(s1,s2,s3,i,j+1,k+1,dp);
-        }
-        else if(s2[j]==s3[k])
-        {
-            return dp[i][j]= solve(s1,s2,s3,i,j+1,k+1,dp);
-        }
-        else if(s1[i]==s3[k])
-        {
-            return dp[i][j]= solve(s1,s2,s3,i+1,j,k+1,dp);
-        }
-        else{
-            return dp[i][j]= false;
-        }
-    }
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
-        return solve(s1,s2,s3,0,0,0,dp);
+        int n1=s1.size(),n2=s2.size(),n3=s3.size();
+        if(n3!=n1+n2)
+        {
+            return 0;
+        }
+
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+
+        for(int i=n1;i>=0;i--)
+        {
+            for(int j=n2;j>=0;j--)
+            {
+                int k=i+j;
+                if(i==n1 && j==n2)
+                {
+                    dp[i][j]=1;
+                }
+                else if(s3[k]==s1[i] && s3[k]==s2[j])
+                {
+                    dp[i][j]=dp[i+1][j] || dp[i][j+1];
+                }
+                else if(s3[k]==s1[i])
+                {
+                    dp[i][j]=dp[i+1][j];
+                }
+                else if(s3[k]==s2[j])
+                {
+                    dp[i][j]=dp[i][j+1];
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
