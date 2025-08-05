9597681a6d45cebc2814b202d9cd0a160290408c
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-08-06 00:50:31 +0530

    Time: 9 ms (86.65%) | Memory: 101.9 MB (88.88%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    void dfs(int current, int distance, const vector<int>& edges, vector<int>& distances) {
-        while (current != -1 && distances[current] == -1) {
-            distances[current] = distance++;
-            current = edges[current];
+    void dfs(int current,int distance,vector<int>&dist,vector<int>&edges)
+    {
+        while(current!=-1 && dist[current]==-1)
+        {
+            dist[current]=distance++;
+            current=edges[current];
         }
     }
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int res=-1;
+        int min_of_max=INT_MAX;
+        int n=edges.size();
 
-    int closestMeetingNode(vector<int>& edges, int start1, int start2) {
-        int res = -1, Min_Of_Max = INT_MAX, n = edges.size();
-        vector<int> dist1(n, -1), dist2(n, -1);
-        dfs(start1, 0, edges, dist1);
-        dfs(start2, 0, edges, dist2);
+        vector<int>dist1(n,-1);
+        vector<int>dist2(n,-1);
 
-        for (int i = 0; i < n; i++) {
-            if (dist1[i] >= 0 && dist2[i] >= 0) {
-                int maxDist = max(dist1[i], dist2[i]);
-                if (maxDist < Min_Of_Max) {
-                    Min_Of_Max = maxDist;
-                    res = i;
+        dfs(node1,0,dist1,edges);
+        dfs(node2,0,dist2,edges);
+
+        for(int i=0;i<n;i++)
+        {
+            if(dist1[i]>=0 && dist2[i]>=0)
+            {
+                int maxi=max(dist1[i],dist2[i]);
+                if(maxi<min_of_max)
+                {
+                    min_of_max=maxi;
+                    res=i;
                 }
             }
         }
