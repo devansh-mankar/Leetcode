35aff1591e462fbb1a9f1497c72e499cadcbfa6f
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2025-08-23 15:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (56.05%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -3,28 +3,29 @@ public:
     int myAtoi(string s) {
         int n=s.size();
         int i=0;
-        bool negative=false;
         long ans=0;
+        bool negative=false;
 
         while(i<n && s[i]==' ')
         {
             i++;
         }
-        if(i<n && (s[i]=='+' || s[i]=='-'))
+        if(i<n && (s[i]=='-' || s[i]=='+'))
         {
             negative=(s[i]=='-');
             i++;
         }
-        while(i<n && (s[i]>='0' && s[i]<='9'))
+        while(i<n && s[i]>='0' && s[i]<='9')
         {
             ans=ans*10+(s[i]-'0');
             if(ans>INT_MAX)
             {
-                return negative?INT_MIN:INT_MAX;
+                return negative? INT_MIN:INT_MAX;
             }
             i++;
         }
         return negative?-ans:ans;
+
 
     }
 };
