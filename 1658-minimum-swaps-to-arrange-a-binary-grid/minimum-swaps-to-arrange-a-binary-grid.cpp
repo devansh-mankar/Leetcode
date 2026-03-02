0ccd29f8089e02dac4b7a0531e36e7de448fd9a6
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-03-02 15:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 30 MB (65.85%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -3,6 +3,7 @@ public:
     int minSwaps(vector<vector<int>>& grid) {
         int n=grid.size();
 
+        //find trailing zeros in each row
         vector<int>trailing(n);
 
         for(int i=0;i<n;i++)
@@ -11,7 +12,7 @@ public:
             for(int j=n-1;j>=0;j--)
             {
                 if(grid[i][j]==0) count++;
-                else break;
+                else break; //no trailing zeros
             }
             trailing[i]=count;
         }
@@ -20,8 +21,8 @@ public:
 
         for(int i=0;i<n;i++)
         {
-            int j=i;
             int required=n-i-1;
+            int j=i;
 
             while(j<n && trailing[j]<required)
             {
@@ -33,11 +34,11 @@ public:
             while(j>i)
             {
                 swap(trailing[j],trailing[j-1]);
-                j--;
                 swaps++;
+                j--;
             }
         }
 
-        return swaps++;
+        return swaps;
     }
 };
