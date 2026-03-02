420b7a6f0b3204f10aa7f97d4fb8d21ccb0882cc
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-03-02 19:16:25 +0530

    Time: 11 ms (14.08%) | Memory: 24 MB (7.95%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        int xorr=0;
+
+        unordered_map<int,int>mp;
+
         for(int i=0;i<n;i++)
         {
-            xorr=xorr^nums[i];
+            mp[nums[i]]++;
         }
-        return xorr;
+
+        for(auto it:mp)
+        {
+            if(it.second==1)
+            {
+                return it.first;
+            }
+        }
+        return -1;
     }
 };
