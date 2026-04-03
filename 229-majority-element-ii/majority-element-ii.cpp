e03165641e3e4a82146540a785dab75dd617faba
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-04-03 23:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (83.12%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,19 +4,58 @@ public:
         int n=nums.size();
         vector<int>ans;
 
-        unordered_map<int,int>mp;
+        int ele1=0;
+        int ele2=0;
+        int count1=0,count2=0;
 
         for(int i=0;i<n;i++)
         {
-            mp[nums[i]]++;
+            if(count1==0 && ele2!=nums[i])
+            {
+                count1=1;
+                ele1=nums[i];
+            }
+            else if(count2==0 && ele1!=nums[i])
+            {
+                count2=1;
+                ele2=nums[i];
+            }
+            else if(ele2==nums[i])
+            {
+                count2++;
+            }
+            else if(ele1==nums[i])
+            {
+                count1++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
 
-        for(auto it:mp)
+        count1=0;
+        count2=0;
+
+        for(int i=0;i<n;i++)
         {
-            if(it.second>(n/3))
+            if(ele1==nums[i])
             {
-                ans.push_back(it.first);
+                count1++;
             }
+            if(ele2==nums[i])
+            {
+                count2++;
+            }
+        }
+
+        if(count1>(n/3))
+        {
+            ans.push_back(ele1);
+        }
+        if(count2>(n/3) && ele1!=ele2)
+        {
+            ans.push_back(ele2);
         }
         return ans;
     }
