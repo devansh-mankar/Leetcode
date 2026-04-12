710627ae5f007f568ff5d84c5dd1fbb148ca3c5d
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-04-12 13:58:54 +0530

    Time: 35 ms (5.03%) | Memory: 73.1 MB (14.88%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int n=nums.size();
-        int maxi=INT_MIN;
-
+    int CrossSumCalc(vector<int>&nums,int left,int mid,int right)
+    {
         int sum=0;
-        for(int i=0;i<n;i++)
+        int lsum=INT_MIN;
+
+        for(int i=mid;i>=left;i--)
         {
             sum+=nums[i];
-            maxi=max(maxi,sum);
-            if(sum<0)
-            {
-                sum=0;
-            }
-            
+            lsum=max(lsum,sum);
         }
-        return maxi;
+
+        sum=0;
+        int rsum=INT_MIN;
+        for(int i=mid+1;i<=right;i++)
+        {
+            sum+=nums[i];
+            rsum=max(rsum,sum);
+        }
+
+        return lsum+rsum;
+    }
+    int maxSubarrayDandQ(vector<int>&nums,int left,int right)
+    {
+        if(left==right)
+        {
+            return nums[left];
+        }
+        int mid=left+(right-left)/2;
+        int leftMax=maxSubarrayDandQ(nums,left,mid);
+        int rightMax=maxSubarrayDandQ(nums,mid+1,right);
+        int crossSum=CrossSumCalc(nums,left,mid,right);
+
+        return max({leftMax,rightMax,crossSum});
+    }
+    int maxSubArray(vector<int>& nums) {
+        return maxSubarrayDandQ(nums,0,nums.size()-1);
     }
 };
