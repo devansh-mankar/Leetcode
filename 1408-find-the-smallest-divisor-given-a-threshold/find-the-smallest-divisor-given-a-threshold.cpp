2ee00a4d5a10b8c3748d9284a1e459735a7cf42a
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-04-25 12:27:37 +0530

    Time: 8 ms (53.47%) | Memory: 25.9 MB (98.01%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    long long calc(vector<int>&nums,int div)
-    {
-        long long sum=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum+=ceil(1.0*nums[i]/div);
-        }
-        return sum;
-    }
     int smallestDivisor(vector<int>& nums, int threshold) {
-     int n=nums.size();
-      int maxi=*max_element(nums.begin(),nums.end());
+        
+        int n=nums.size();
 
-      
-      int low=1,high=maxi;
-      while(low<=high)
-      {
-        int mid=low+(high-low)/2;
-        long long sum=calc(nums,mid);
-        if(sum<=threshold)
-        {
-            high=mid-1;
-        }
-        else
-        {
-            low=mid+1;
-        }
-      }
-      return low;
+        int low=1;
+        int high=*max_element(nums.begin(),nums.end());
 
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int sum=0;
+            for(int i=0;i<n;i++)
+            {
+                sum+=(nums[i]+mid-1)/mid;
+            }
+
+            if(sum<=threshold)
+            {
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
     }
 };
