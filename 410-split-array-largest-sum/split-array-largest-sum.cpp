ffe48790db1cb3171423fd4a3d610294dc760d42
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-04-26 15:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (40.74%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    int countSubarrays(vector<int>&nums,int limit)
-    {
-        //sort(nums.begin(),nums.end());
-        int count=1;
-        int n=nums.size();
-        long long sum=0;
-        for(int i=0;i<n;i++)
-        {
-            if(sum+nums[i]<=limit)
-            {
-                sum+=nums[i];
-            }
-            else
-            {
-                count++;
-                sum=nums[i];
-            }
-        }
-        return count;
-        
-    }
     int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
+
         int low=*max_element(nums.begin(),nums.end());
         int high=accumulate(nums.begin(),nums.end(),0);
+        int res=1;
 
         while(low<=high)
         {
             int mid=low+(high-low)/2;
-            if(countSubarrays(nums,mid)>k)
+            int count=1;
+            long long sum=0;
+            for(int i=0;i<n;i++)
             {
-                low=mid+1;
+                if(sum+nums[i]>mid)
+                {
+                    count++;
+                    sum=nums[i];
+                }
+                else{
+                    sum+=nums[i];
+                }
+                if(count>k) break;
             }
-            else
+            if(count<=k)
             {
+                res=mid;
                 high=mid-1;
             }
+            else{
+                low=mid+1;
+            }
         }
-        return low;
+        return res;
     }
 };
