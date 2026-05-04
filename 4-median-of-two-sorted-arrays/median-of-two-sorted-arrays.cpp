9968992a1750edbe72ed07ec56e44790ee6feaf6
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-05-04 15:47:40 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (94.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,39 +1,60 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-         vector<int>temp;
-        
+        int n1=nums1.size();
+        int n2=nums2.size();
+
         int i=0;
         int j=0;
-        int n=nums1.size();
-        int m=nums2.size();
-        
-        while(i<n && j<m)
+        int count=0;
+
+        int n=n1+n2;
+
+        int ind2=n/2;
+        int ind1=n/2-1;
+        int ind1el=-1;
+        int ind2el=-1;
+
+        while(i<n1 && j<n2)
         {
             if(nums1[i]<nums2[j])
             {
-                temp.push_back(nums1[i++]);
+                if(count==ind1) ind1el=nums1[i];
+                if(count==ind2) ind2el=nums1[i];
+                i++;
+                count++;
             }
             else{
-                temp.push_back(nums2[j++]);
+                if(count==ind1) ind1el=nums2[j];
+                if(count==ind2) ind2el=nums2[j];
+                j++;
+                count++;
             }
         }
-        
-        while(i<n)
+
+        while(i<n1)
         {
-            temp.push_back(nums1[i++]);
-        }
-        while(j<m)
-        {
-            temp.push_back(nums2[j++]);
+            if(count==ind1) ind1el=nums1[i];
+            if(count==ind2) ind2el=nums1[i];
+            i++;
+            count++;
         }
 
-        int size=n+m;
-        if(size%2==1)
+        while(j<n2)
         {
-            return (double)temp[size/2];
+            if(count==ind1) ind1el=nums2[j];
+            if(count==ind2) ind2el=nums2[j];
+            j++;
+            count++;
         }
-        return ((double)temp[size/2]+(double)temp[size/2-1])/2.0;
+
+        
+        if(n%2==1)
+        {
+            return (double)ind2el;
+        }
+        return (double)(ind1el+ind2el)/2.0;
+
+
     }
 };
