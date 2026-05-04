ffc2f0a68a07a52f64029c3777e7fa53a2abf0c1
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-05-04 23:59:47 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (94.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,38 +4,55 @@ public:
         int n1=nums1.size();
         int n2=nums2.size();
 
-        vector<int>temp;
-
-        int n=n1+n2;
-
         int i=0;
         int j=0;
+        int count=0;
+        int n=n1+n2;
+
+
+        int ind2=n/2;
+        int ind1=n/2-1;
+        int ind1el=-1;
+        int ind2el=-1;
+
 
         while(i<n1 && j<n2)
         {
             if(nums1[i]<nums2[j])
             {
-                temp.push_back(nums1[i++]);
+                if(ind1==count) ind1el=nums1[i];
+                if(ind2==count) ind2el=nums1[i];
+                i++;
+                count++;
             }
             else{
-                temp.push_back(nums2[j++]);
+                if(ind1==count) ind1el=nums2[j];
+                if(ind2==count) ind2el=nums2[j];
+                j++;
+                count++;
             }
         }
 
         while(i<n1)
         {
-            temp.push_back(nums1[i++]);
+            if(ind1==count) ind1el=nums1[i];
+            if(ind2==count) ind2el=nums1[i];
+            i++;
+            count++;
         }
 
         while(j<n2)
         {
-            temp.push_back(nums2[j++]);
+            if(ind1==count) ind1el=nums2[j];
+            if(ind2==count) ind2el=nums2[j];
+            j++;
+            count++;
         }
 
         if(n%2==1)
         {
-            return (double)temp[n/2];
+            return (double)ind2el;
         }
-        return ((double)temp[n/2]+(double)temp[n/2-1])/2.0;
+        return ((double)ind1el+(double)ind2el)/2.0;
     }
 };
