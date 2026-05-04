afef72a122f799d528d5080663a28b5096beb3d3
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-05-04 16:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (60.43%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,57 +4,44 @@ public:
         int n1=nums1.size();
         int n2=nums2.size();
 
-        int i=0;
-        int j=0;
-        int count=0;
+        if(n1>n2)
+        {
+            return findMedianSortedArrays(nums2,nums1);
+        }
 
         int n=n1+n2;
+        int low=0;
+        int high=n1;
 
-        int ind2=n/2;
-        int ind1=n/2-1;
-        int ind1el=-1;
-        int ind2el=-1;
-
-        while(i<n1 && j<n2)
+        while(low<=high)
         {
-            if(nums1[i]<nums2[j])
+            int mid1=low+(high-low)/2;
+            int mid2=(n1+n2+1)/2-mid1;
+
+            int l1= mid1==0 ? INT_MIN : nums1[mid1-1];
+            int l2= mid2==0 ? INT_MIN : nums2[mid2-1];
+
+            int r1= mid1==n1 ? INT_MAX : nums1[mid1];
+            int r2= mid2==n2 ? INT_MAX : nums2[mid2];
+
+            if(l1<=r2 && l2<=r1)
             {
-                if(count==ind1) ind1el=nums1[i];
-                if(count==ind2) ind2el=nums1[i];
-                i++;
-                count++;
+                if((n%2)==1)
+                {
+                    return (double)max(l1,l2);
+                }
+                else{
+                    return (double)(max(l1,l2)+min(r1,r2))/2.0;
+                }
+            }
+            else if(l1>r2)
+            {
+                high=mid1-1;
             }
             else{
-                if(count==ind1) ind1el=nums2[j];
-                if(count==ind2) ind2el=nums2[j];
-                j++;
-                count++;
+                low=mid1+1;
             }
         }
-
-        while(i<n1)
-        {
-            if(count==ind1) ind1el=nums1[i];
-            if(count==ind2) ind2el=nums1[i];
-            i++;
-            count++;
-        }
-
-        while(j<n2)
-        {
-            if(count==ind1) ind1el=nums2[j];
-            if(count==ind2) ind2el=nums2[j];
-            j++;
-            count++;
-        }
-
-        
-        if(n%2==1)
-        {
-            return (double)ind2el;
-        }
-        return (double)(ind1el+ind2el)/2.0;
-
-
+        return 0;
     }
 };
