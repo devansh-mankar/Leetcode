5d0159e40e6708776112a2c96dca05baa98090b2
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-06-03 12:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (19.79%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num<=1)
+        {
+            return 0;
+        }
+        int sum=1;
+        for(int i=2;i*i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                sum+=i;
+                if(num/i!=i) sum+=(num/i);
+            }
+        }
+        return sum==num;
+    }
+};
