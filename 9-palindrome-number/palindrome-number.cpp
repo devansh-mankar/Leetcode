f00276adc6df216b7ebad236fd7c3499a664e02e
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-07-31 15:06:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (65.68%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        {
+            return 0;
+        }
+        long long rev=0;
+
+        int num=x;
+
+        while(x)
+        {
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        return (int)rev==num;
+    }
+};
