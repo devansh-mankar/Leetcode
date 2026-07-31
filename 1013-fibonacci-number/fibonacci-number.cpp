88a43b343d40eff7965fcdad379f0955e38e1aa0
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-07-31 18:39:46 +0530

    Time: 13 ms (31.68%) | Memory: 7.6 MB (89.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+    int solve(int n)
+    {
+        if(n<=1) return n;
+        return solve(n-1)+solve(n-2);
+    }
     int fib(int n) {
-        if(n<=1)
-        {
-            return n;
-        }
-       return fib(n-1)+fib(n-2);
+        return solve(n);
     }
 };
