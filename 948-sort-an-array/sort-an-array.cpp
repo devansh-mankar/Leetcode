abd5a3294677c308a4798f9178c491614f281359
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-07 16:26:05 +0530

    Time: 1727 ms (5.00%) | Memory: 71.2 MB (71.10%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,40 @@
 class Solution {
 public:
+    int partition(vector<int>&nums,int low,int high)
+    {
+        int i=low-1;
+        int j=high+1;
+
+        int pivot=nums[low];
+
+        while(1)
+        {
+            do{
+                i++;
+            }while(nums[i]<pivot);
+
+            do{
+                j--;
+            }while(nums[j]>pivot);
+
+            if(i>=j) return j;
+
+            swap(nums[i],nums[j]);
+        }
+    }
+    void HoareQuickSort(vector<int>&nums,int low,int high)
+    {
+        if(low>=high) return;
+
+        int part=partition(nums,low,high);
+        HoareQuickSort(nums,low,part);
+        HoareQuickSort(nums,part+1,high);
+    }
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+
+        HoareQuickSort(nums,0,n-1);
+
         return nums;
     }
 };
