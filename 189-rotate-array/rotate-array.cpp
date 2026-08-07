e14fb0a11f422ef18101fb11969401fe8874f81a
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-07 20:26:53 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (95.17%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,6 +4,7 @@ public:
         int n=nums.size();
 
         k=k%n;
+
         reverse(nums.begin(),nums.begin()+(n-k));
         reverse(nums.begin()+(n-k),nums.end());
         reverse(nums.begin(),nums.end());
