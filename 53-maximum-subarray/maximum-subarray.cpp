c4f28f10b98bfad7fcb54537415f33c7ca0a610f
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-08 22:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (81.99%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    int CrossSumCalc(vector<int>&nums,int left,int mid,int right)
-    {
-        int sum=0;
-        int lsum=INT_MIN;
-
-        for(int i=mid;i>=left;i--)
-        {
-            sum+=nums[i];
-            lsum=max(lsum,sum);
-        }
-
-        sum=0;
-        int rsum=INT_MIN;
-        for(int i=mid+1;i<=right;i++)
-        {
-            sum+=nums[i];
-            rsum=max(rsum,sum);
-        }
-
-        return lsum+rsum;
-    }
-    int maxSubarrayDandQ(vector<int>&nums,int left,int right)
-    {
-        if(left==right)
-        {
-            return nums[left];
-        }
-        int mid=left+(right-left)/2;
-        int leftMax=maxSubarrayDandQ(nums,left,mid);
-        int rightMax=maxSubarrayDandQ(nums,mid+1,right);
-        int crossSum=CrossSumCalc(nums,left,mid,right);
-
-        return max({leftMax,rightMax,crossSum});
-    }
     int maxSubArray(vector<int>& nums) {
-        return maxSubarrayDandQ(nums,0,nums.size()-1);
+        int n=nums.size();
+
+        int maxi=INT_MIN;
+        int sum=0;
+
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            maxi=max(maxi,sum);
+            if(sum<0)
+            {
+                sum=0;
+            }
+            
+        }
+
+        return maxi;
+
     }
 };
