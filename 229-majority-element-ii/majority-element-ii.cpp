18943bfd242f12fbec0249e950d5352fefd96348
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-08 22:19:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (42.83%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,54 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        //extended Boyer moree voting algorithm
+        vector<int>ans;
+
         int n=nums.size();
 
-        unordered_map<int,int>mp;
-        vector<int>ans;
+        int ele1=0;
+        int ele2=0;
+        int count1=0;
+        int count2=0;
+
         for(int i=0;i<n;i++)
         {
-            mp[nums[i]]++;
+            if(count1==0 && ele2!=nums[i])
+            {
+                count1=1;
+                ele1=nums[i];
+            }
+            else if(count2==0 && ele1!=nums[i])
+            {
+                count2=1;
+                ele2=nums[i];
+            }
+            else if(ele1==nums[i])
+            {
+                count1++;
+            }
+            else if(ele2==nums[i])
+            {
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
-        
-        for(auto it:mp)
+
+        count1=0;
+        count2=0;
+
+        for(int i=0;i<n;i++)
         {
-            if(it.second>n/3) ans.push_back(it.first);
+            if(nums[i]==ele1) count1++;
+            if(nums[i]==ele2) count2++;
         }
+
+        if(count1>(n/3)) ans.push_back(ele1);
+        if(count2>(n/3) && ele1!=ele2) ans.push_back(ele2);
+
         return ans;
     }
 };
