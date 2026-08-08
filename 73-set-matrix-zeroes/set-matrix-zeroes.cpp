f23216838182630007f33de0608617d3d78e5817
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-08 23:02:15 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (33.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,36 +3,22 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-
-        bool firstRow=false;
-        bool firstCol=false;
+        int col0=1;
 
         for(int i=0;i<n;i++)
         {
-            if(matrix[i][0]==0)
-            {
-                firstCol=true;
-                break;
-            }
-        }
-
-        for(int i=0;i<m;i++)
-        {
-            if(matrix[0][i]==0)
-            {
-                firstRow=true;
-                break;
-            }
-        }
-
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
+            for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
                     matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    if(j!=0)
+                    {
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
                 }
             }
         }
@@ -48,15 +34,14 @@ public:
             }
         }
 
-        if(firstRow)
+        if(matrix[0][0]==0)
         {
-            for(int i=0;i<m;i++)
+            for(int j=0;j<m;j++)
             {
-                matrix[0][i]=0;
+                matrix[0][j]=0;
             }
         }
-
-        if(firstCol)
+        if(col0==0)
         {
             for(int i=0;i<n;i++)
             {
