4ba7c3207d177bea3c784babcdb6996a2ee1cb2a
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-12 11:17:25 +0530

    Time: 2 ms (9.48%) | Memory: 9 MB (50.83%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int nCr(int n,int r)
-    {
-        long long res=1;
-        for(int i=0;i<r;i++)
-        {
-            res=res*(n-i);
-            res=res/(i+1);
-        }
-        return res;
-    }
     vector<int> getRow(int rowIndex) {
+        long long prev=1;
         vector<int>ans;
-        
-        for(int i=0;i<=rowIndex;i++)
+        ans.push_back(1);
+
+        for(int i=1;i<=rowIndex;i++)
         {
-            ans.push_back(nCr(rowIndex,i));
+            long long next=prev*(rowIndex-i+1)/i;
+            ans.push_back(next);
+            prev=next;
         }
         return ans;
     }
