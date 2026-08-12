0ef764da299217d7b55d7f25ed6ca3d22da9f7bd
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-12 20:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (36.10%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -9,10 +9,8 @@ public:
         while(low<high)
         {
             int mid=low+(high-low)/2;
-            if(mid%2==1)
-            {
-                mid--;
-            }
+            if(mid%2==1) mid--;
+
             if(nums[mid]==nums[mid+1])
             {
                 low=mid+2;
