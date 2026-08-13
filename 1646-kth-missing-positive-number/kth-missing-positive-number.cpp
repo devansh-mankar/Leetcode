fdf2de5ca5cb916203f95d3cf90105a627362690
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-13 20:42:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (29.71%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -10,7 +10,7 @@ public:
         {
             int mid=low+(high-low)/2;
             int missing=arr[mid]-(mid+1);
-
+            
             if(missing<k)
             {
                 low=mid+1;
@@ -18,7 +18,7 @@ public:
             else{
                 high=mid-1;
             }
-        }   
+        }
         return k+high+1;
     }
 };
