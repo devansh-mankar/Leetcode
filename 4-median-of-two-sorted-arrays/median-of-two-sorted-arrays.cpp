d0e699c562c0b985918eeb5187c2c4745104af16
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-14 23:41:24 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.69%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,33 +4,54 @@ public:
         int n1=nums1.size();
         int n2=nums2.size();
 
-        vector<int>temp;
         int i=0;
         int j=0;
+        int n=(n1+n2);
+
+        int ele1=-1;
+        int ele2=-1;
+        int count=0;
+        int ind1=n/2;
+        int ind2=ind1-1;
+
         while(i<n1 && j<n2)
         {
             if(nums1[i]<=nums2[j])
             {
-                temp.push_back(nums1[i++]);
+                if(count==ind1) ele1=nums1[i];
+                if(count==ind2) ele2=nums1[i];
+                count++;
+                i++;
             }
             else{
-                temp.push_back(nums2[j++]);
+                if(count==ind1) ele1=nums2[j];
+                if(count==ind2) ele2=nums2[j];
+                count++;
+                j++;
             }
         }
 
         while(i<n1)
         {
-            temp.push_back(nums1[i++]);
+            if(count==ind1) ele1=nums1[i];
+            if(count==ind2) ele2=nums1[i];
+            i++;
+            count++;
         }
+
         while(j<n2)
         {
-            temp.push_back(nums2[j++]);
+            if(count==ind1) ele1=nums2[j];
+            if(count==ind2) ele2=nums2[j];
+            j++;
+            count++;
         }
-        int n=temp.size();
-        if((n1+n2)%2==1)
+
+        if(n%2==1)
         {
-            return (double)temp[n/2];
+            return (double)(ele1);
         }
-        return (double)(temp[n/2]+temp[n/2-1])/2;
+        return (double)(ele1+ele2)/2;
+
     }
 };
