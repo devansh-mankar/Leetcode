83cb95318c92911f3ca86a2bee78c0e554bb6634
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-14 18:34:43 +0530

    Time: 8 ms (5.79%) | Memory: 96.1 MB (9.70%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,58 +1,24 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
+        int n=nums1.size();
+        int m=nums2.size();
 
-        int i=0;
-        int j=0;
-        int count=0;
-        int n=n1+n2;
-
-
-        int ind2=n/2;
-        int ind1=n/2-1;
-        int ind1el=-1;
-        int ind2el=-1;
-
-
-        while(i<n1 && j<n2)
+        vector<int>temp;
+        for(int i=0;i<n;i++)
         {
-            if(nums1[i]<nums2[j])
-            {
-                if(ind1==count) ind1el=nums1[i];
-                if(ind2==count) ind2el=nums1[i];
-                i++;
-                count++;
-            }
-            else{
-                if(ind1==count) ind1el=nums2[j];
-                if(ind2==count) ind2el=nums2[j];
-                j++;
-                count++;
-            }
+            temp.push_back(nums1[i]);
         }
-
-        while(i<n1)
+        for(int j=0;j<m;j++)
         {
-            if(ind1==count) ind1el=nums1[i];
-            if(ind2==count) ind2el=nums1[i];
-            i++;
-            count++;
+            temp.push_back(nums2[j]);
         }
-
-        while(j<n2)
+        sort(temp.begin(),temp.end());
+        int size=temp.size();
+        if(size%2==1)
         {
-            if(ind1==count) ind1el=nums2[j];
-            if(ind2==count) ind2el=nums2[j];
-            j++;
-            count++;
+            return (double)temp[size/2];
         }
-
-        if(n%2==1)
-        {
-            return (double)ind2el;
-        }
-        return ((double)ind1el+(double)ind2el)/2.0;
+        return (double)(temp[size/2-1]+temp[size/2])/2;
     }
 };
