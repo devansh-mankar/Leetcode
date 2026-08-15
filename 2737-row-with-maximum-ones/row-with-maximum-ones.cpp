90ac8c4fc09f997d77ad5ae66f6245528493fce2
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-15 16:10:42 +0530

    Time: 10 ms (15.78%) | Memory: 64.6 MB (93.84%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,47 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int>&arr,int k)
-    {
-        int n=arr.size();
-        int low=0;
-        int high=n-1;
-        int res=n;
-
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(arr[mid]>=k)
-            {
-                res=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return res;
-    }
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-
-        for(int i=0;i<n;i++)
-        {
-            sort(mat[i].begin(),mat[i].end());
-        }
-
-        int cntMax=-1;
+        int maxi=-1;
         int index=-1;
-
         for(int i=0;i<n;i++)
         {
-            int count=m-solve(mat[i],1);
-            if(cntMax<count)
+            int count=0;
+            for(int j=0;j<m;j++)
             {
-                cntMax=count;
+                if(mat[i][j]==1)
+                {
+                    count++;
+                }
+            }
+            if(maxi<count)
+            {
+                maxi=count;
                 index=i;
             }
         }
-        return {index,cntMax};
+        return {index,maxi};
     }
 };
