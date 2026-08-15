4b9705a031c2ad1c7d210d348c0456a283d0950c
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-15 23:38:36 +0530

    Time: 5 ms (15.37%) | Memory: 10.7 MB (40.90%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,33 +2,32 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n=s.size();
-        vector<string>temp;
-        string str="";
+        stack<string>st;
 
         int i=0;
-
         while(i<n)
         {
             while(i<n && s[i]==' ')
             {
                 i++;
             }
+            string str="";
             while(i<n && s[i]!=' ')
             {
                 str+=s[i++];
             }
-            if(str!="")
+            if(!str.empty())
             {
-                temp.push_back(str);
-                str="";
+                st.push(str);
             }
         }
 
         string ans="";
-        for(int i=temp.size()-1;i>=0;i--)
+        while(!st.empty())
         {
-            ans+=temp[i];
-            if(i!=0)
+            ans+=st.top();
+            st.pop();
+            if(!st.empty())
             {
                 ans+=" ";
             }
