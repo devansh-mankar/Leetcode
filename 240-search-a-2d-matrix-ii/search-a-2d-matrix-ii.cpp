b42bec879194863c0cf07958da2430f57eadcac3
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-15 15:13:09 +0530

    Time: 48 ms (78.65%) | Memory: 18.4 MB (99.69%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
+    bool solve(int row,int col,vector<vector<int>>&matrix,int k)
+    {
+        if(row>=matrix.size() || col<0) return 0;
+        if(matrix[row][col]>k) return solve(row,col-1,matrix,k);
+        else if(matrix[row][col]<k) return solve(row+1,col,matrix,k);
+
+        return matrix[row][col]==k;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();
         int m=matrix[0].size();
 
-        int row=0;
-        int col=m-1;
-        //think of it as some form of spiral matrix we are reaching there by reducing the row or column
-        while(row<n && col>=0)
-        {
-            if(matrix[row][col]==target)
-            {
-                return true;
-            }
-            else if(matrix[row][col]>target)
-            {
-                col--;
-            }
-            else{
-                row++;
-            }
-        }
-        return false;
-
+        return solve(0,m-1,matrix,target);
     }
 };
