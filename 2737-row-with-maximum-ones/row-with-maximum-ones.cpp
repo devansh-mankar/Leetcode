6d8658b7e9c24edebfdfe787071ee8217adf95b3
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-15 16:15:57 +0530

    Time: 2 ms (65.90%) | Memory: 64.8 MB (38.55%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
+    int findOnes(vector<int>&arr,int k)
+    {
+        int low=0;
+        int high=arr.size()-1;
+        int res=arr.size();
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=k)
+            {
+                res=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return res;
+    }
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            sort(mat[i].begin(),mat[i].end());
+        }
         int maxi=-1;
         int index=-1;
         for(int i=0;i<n;i++)
         {
-            int count=0;
-            for(int j=0;j<m;j++)
-            {
-                if(mat[i][j]==1)
-                {
-                    count++;
-                }
-            }
+            int count=m-findOnes(mat[i],1);
             if(maxi<count)
             {
                 maxi=count;
