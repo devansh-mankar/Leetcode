3cfcfb8ccd7b89ca5830b53c48cc993af929f533
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-16 15:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (38.81%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,24 +4,16 @@ public:
         int n=strs.size();
 
         sort(strs.begin(),strs.end());
-
         string first=strs[0];
         string last=strs[n-1];
 
-        string ans="";
         for(int i=0;i<first.size();i++)
         {
-            if(first[i]==last[i])
+            if(first[i]!=last[i])
             {
-                ans+=first[i];
-            }
-            else{
-                break;
+                return first.substr(0,i);
             }
         }
-        return ans;
-
-
-
+        return first;
     }
 };
