7717a91c7ba64e5fe56bcfaf9ec3a7439d6ae6e6
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-18 11:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (5.01%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,44 +11,37 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next) return head;
 
-        if(head==NULL)
-        {
-            return NULL;
-        }
         ListNode* temp=head;
-        vector<int>arr;
-        //storing odd indexes
+        vector<int>v;
         while(temp && temp->next)
         {
-            arr.push_back(temp->val);
+            v.push_back(temp->val);
             temp=temp->next->next;
         }
-        //if we have odd length linked list we need to check for the last node
-        if(temp)
-        {
-            arr.push_back(temp->val);
-        }
-        //storing even indexes
+        //if length of linked list is odd for last index
+        if(temp) v.push_back(temp->val);
+
         temp=head->next;
         while(temp && temp->next)
         {
-            arr.push_back(temp->val);
+            v.push_back(temp->val);
             temp=temp->next->next;
         }
-        if(temp)
-        {
-            arr.push_back(temp->val);
-        }
+        //if length of linked list is even for last index
+        if(temp) v.push_back(temp->val);
 
-        temp=head;
+
         int i=0;
+        temp=head;
         while(temp)
         {
-            temp->val=arr[i++];
+            temp->val=v[i++];
             temp=temp->next;
         }
         return head;
+
 
     }
 };
