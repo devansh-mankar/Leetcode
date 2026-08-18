bc7e6ce094e13eda1295437236f34da0210c2963
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-18 16:17:52 +0530

    Time: 11 ms (81.32%) | Memory: 58.1 MB (61.12%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,72 +10,24 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* left,ListNode* right)
-    {
-        if(!left) return right;
-        if(!right) return left;
-
-        ListNode* temp=new ListNode(0);
-        ListNode* ans=temp;
-
-        while(left && right)
-        {
-            if(left->val<=right->val)
-            {
-                temp->next=left;
-                temp=temp->next;
-                left=left->next;
-            }
-            else{
-                temp->next=right;
-                temp=temp->next;
-                right=right->next;
-            }
-        }
-
-        while(left)
-        {
-            temp->next=left;
-            temp=temp->next;
-            left=left->next;
-        }
-
-        while(right)
-        {
-            temp->next=right;
-            temp=temp->next;
-            right=right->next;
-        }
-
-        return ans->next;
-    }
-    ListNode* getMiddle(ListNode* head)
-    {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        ListNode* prev=NULL;
-
-        while(fast && fast->next)
-        {
-            prev=slow;
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return prev;
-    }
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next)
+        vector<int>v;
+
+        ListNode* temp=head;
+        while(temp)
         {
-            return head;
+            v.push_back(temp->val);
+            temp=temp->next;
         }
-        ListNode* mid=getMiddle(head);
-        ListNode* left=head;
-        ListNode* right=mid->next;
-        mid->next=NULL;
 
-        left=sortList(left);
-        right=sortList(right);
+        sort(v.begin(),v.end());
 
-        return merge(left,right);
+        temp=head;
+        for(int i=0;i<v.size();i++)
+        {
+            temp->val=v[i];
+            temp=temp->next;
+        }
+        return head;
     }
 };
