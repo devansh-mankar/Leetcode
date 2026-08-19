96fb8d3bc2c4f40f69dcdf628c9814c39a4d5507
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-19 15:25:04 +0530

    Time: 0 ms (100.00%) | Memory: 125.5 MB (93.85%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -11,28 +11,26 @@
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        ListNode* prev=NULL;
+        
         ListNode* curr=head;
+        ListNode* prev=NULL;
 
         while(curr)
         {
             int val=2*curr->val;
-
             if(val<10)
             {
                 curr->val=val;
             }
-
             else if(prev!=NULL)
             {
                 curr->val=val%10;
-                prev->val=prev->val+1;
+                prev->val+=1;
             }
-
-            else
-            {
-                //first node we need to create a new node
-                head=new ListNode(1,curr);
+            else{
+                ListNode* newNode=new ListNode(1);
+                newNode->next=head;
+                head=newNode;
                 curr->val=val%10;
             }
             prev=curr;
