9c7d7514deba50cce0ccc91bf3ae1624fdafacb1
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-08-21 11:47:32 +0530

    Time: 2 ms (66.04%) | Memory: 125.5 MB (93.58%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -10,32 +10,54 @@
  */
 class Solution {
 public:
-    ListNode* doubleIt(ListNode* head) {
-        if(!head) return NULL;
-
-        ListNode* temp=head;
+    ListNode* Reverse(ListNode* head)
+    {
         ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* forward=NULL;
+
+        while(curr)
+        {
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+    ListNode* doubleIt(ListNode* head) {
+        if(!head)
+        {
+            return NULL;
+        }
+        head=Reverse(head);
+
+        int carry=0;
+        ListNode* temp=head;
+        ListNode* last=NULL;
         while(temp)
         {
-            int value=temp->val*2;
+            int value=temp->val*2+carry;
+            temp->val=value%10;
             if(value<10)
             {
-                temp->val=value;
-            }
-            else if(prev!=NULL)
-            {
-                temp->val=value%10;
-                prev->val+=1;
+                carry=0;
             }
             else{
-                ListNode* newNode=new ListNode(1);
-                newNode->next=head;
-                head=newNode;
-                temp->val=value%10;
+                carry=1;
             }
-            prev=temp;
+            last=temp;
             temp=temp->next;
         }
+
+
+        if(carry!=0)
+        {
+            ListNode* newNode=new ListNode(carry);
+            last->next=newNode;
+        }
+
+        head=Reverse(head);
         return head;
     }
 };
