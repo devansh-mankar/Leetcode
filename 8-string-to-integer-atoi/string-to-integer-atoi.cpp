617661da6fb7c37a07c392fc74d3645d9a3df3aa
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-16 17:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (18.49%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int n=s.size();
-        bool sign=0;
-        long long num=0;
+    long long solve(int index,string& s,bool& sign,long long res)
+    {
+        if(index>=s.size())
+        {
+            return sign ? -res:res;
+        }
+       
+        if(s[index]>='0' && s[index]<='9')
+        {
 
-        int i=0;
-        while(i<n && s[i]==' ')
-        {
-            i++;
-        }
-        if(i<n && (s[i]=='-' || s[i]=='+'))
-        {
-            sign=(s[i]=='-');
-            i++;
-        }
-        while(i<n && (s[i]>='0' && s[i]<='9'))
-        {
-            num=num*10+(s[i]-'0');
-            if(num>INT_MAX)
+             if((res*10+(s[index]-'0'))>INT_MAX)
             {
                 return sign ? INT_MIN:INT_MAX;
             }
+            return solve(index+1,s,sign,res*10+(s[index]-'0'));
+        }
+        return sign ? -res:res;
+    }
+    int myAtoi(string s) {
+        int n=s.size();
+
+        int i=0;
+        while(i<n && s[i]==' ') i++;
+
+        long long res=0;
+        bool sign=0;
+
+        if(s[i]=='-' || s[i]=='+')
+        {
+            sign = (s[i]=='-') ? 1:0;
             i++;
         }
-        return sign ? -num:num;
+        return (int)solve(i,s,sign,res);
     }
 };
