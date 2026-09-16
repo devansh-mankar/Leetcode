51cef906f3fcee9fda7890533172d59f1ac9dcdd
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-16 17:48:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (53.73%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    long long solve(int index,string& s,bool& sign,long long res)
+    long long solve(int index,string& s,bool sign,long long res)
     {
         if(index>=s.size())
         {
             return sign ? -res:res;
         }
-       
         if(s[index]>='0' && s[index]<='9')
         {
-             if((res*10+(s[index]-'0'))>INT_MAX)
+            if(res*10+(s[index]-'0')>INT_MAX)
             {
                 return sign ? INT_MIN:INT_MAX;
             }
@@ -19,18 +18,18 @@ public:
     }
     int myAtoi(string s) {
         int n=s.size();
-
         int i=0;
+
         while(i<n && s[i]==' ') i++;
 
-        long long res=0;
         bool sign=0;
-
         if(s[i]=='-' || s[i]=='+')
         {
-            sign = (s[i]=='-') ? 1:0;
+            sign =(s[i]=='-' ? 1:0);
             i++;
         }
+
+        long long res=0;
         return (int)solve(i,s,sign,res);
     }
 };
