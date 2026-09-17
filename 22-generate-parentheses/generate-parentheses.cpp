b31368cb86dc814ca0d9469fe72f8519be5f7e78
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-17 17:25:48 +0530

    Time: 43 ms (5.02%) | Memory: 45.7 MB (5.01%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    void solve(int n,int open,int close,string curr,vector<string>&res)
+    bool isValid(string& s)
     {
-        if(curr.size()==2*n)
+        int count=0;
+
+        for(auto c:s)
         {
-            res.push_back(curr);
+            if(c=='(') count++;
+            if(c==')') count--;
+            if(count<0) return 0;
+        }
+        return count==0;
+    }
+    void solve(int n,string curr,vector<string>&res)
+    {
+        if(n*2==curr.size())
+        {
+            if(isValid(curr))
+            {
+                res.push_back(curr);
+            }
             return;
         }
-        if(open<n) solve(n,open+1,close,curr+'(',res);
-        if(close<open) solve(n,open,close+1,curr+')',res);
+        solve(n,curr+'(',res);
+        solve(n,curr+')',res);
     }
     vector<string> generateParenthesis(int n) {
         vector<string>res;
         string curr="";
-        solve(n,0,0,curr,res);
+        solve(n,curr,res);
         return res;
     }
 };
