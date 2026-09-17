3b21e2509b80e24916ecd7ab5ce1389e40568385
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-17 10:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (37.66%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -3,20 +3,20 @@ public:
     int mod=1e9+7;
     long long solve(long long x,long long n)
     {
-        if(n==0) return 1;
+        if(x==1 || n==0) return 1;
 
         if(n%2==0)
         {
             long long x1=(x*x)%mod;
             return solve(x1,n/2)%mod;
         }
-
-        return (x*solve(x,n-1))%mod;
+        return x*solve(x,n-1)%mod;
     }
     int countGoodNumbers(long long n) {
-        long long even=n/2+n%2;
         long long odd=n/2;
+        long long even=n/2+n%2;
+        if(n==1) return 5;
 
-        return (solve(5,even)*solve(4,odd))%mod;
+        return (solve(4,odd)*solve(5,even))%mod;
     }
 };
