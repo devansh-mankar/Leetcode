1c18ca392a77bf8c36c5675256f2eb239a2997ee
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-18 17:32:16 +0530

    Time: 3 ms (98.72%) | Memory: 20.9 MB (79.90%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void solve(int n,string& curr,vector<string>&res)
+    void solve(int index,int n,string& curr,vector<string>&res)
     {
-        if(curr.size()==n)
+        if(index==n)
         {
             res.push_back(curr);
             return;
         }
+
         curr.push_back('1');
-        solve(n,curr,res);
+        solve(index+1,n,curr,res);
         curr.pop_back();
 
         if(curr.empty() || curr.back()!='0')
         {
             curr.push_back('0');
-            solve(n,curr,res);
+            solve(index+1,n,curr,res);
             curr.pop_back();
         }
-        
     }
     vector<string> validStrings(int n) {
         vector<string>res;
         string curr="";
-        solve(n,curr,res);
+        solve(0,n,curr,res);
         return res;
     }
 };
