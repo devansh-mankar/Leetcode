25bb591c18c9b6eaa1fe6603d62729a75472e981
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-18 18:29:21 +0530

    Time: 2 ms (43.45%) | Memory: 13.9 MB (84.09%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(int index,int n,vector<int>&nums,int target,vector<int>&temp,vector<vector<int>>&ans)
+    {
+        if(target==0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        if(target<0) return;
+
+        for(int i=index;i<n;i++)
+        {
+            if(i>index && nums[i]==nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            solve(i+1,n,nums,target-nums[i],temp,ans);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int>temp;
+        solve(0,candidates.size(),candidates,target,temp,ans);
+        return ans;
+    }
+};
