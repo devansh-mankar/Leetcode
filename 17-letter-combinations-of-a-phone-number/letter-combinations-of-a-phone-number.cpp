ad485dd7c8e6c916a9187af59dfa3904e3de59f4
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-19 10:15:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (76.80%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    void solve(int index,int n,string& temp,vector<string>&ans,vector<string>&num,string& digits)
+    void solve(int index,int n,string& digits,vector<string>&num,string& temp,vector<string>&ans)
     {
         if(index==n)
         {
             ans.push_back(temp);
             return;
         }
-        int value=digits[index]-'0';
-        string s=num[value];
+
+        int val=digits[index]-'0';
+        string s=num[val];
 
         for(int i=0;i<s.size();i++)
         {
             temp.push_back(s[i]);
-            solve(index+1,n,temp,ans,num,digits);
+            solve(index+1,n,digits,num,temp,ans);
             temp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
         vector<string>ans;
         string temp="";
-        int n=digits.size();
         vector<string>num={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(0,n,temp,ans,num,digits);
+        int n=digits.size();
+        solve(0,n,digits,num,temp,ans);
         return ans;
     }
 };
