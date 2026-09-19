e55cb94e7c00f04b22ff6f5acfc1f1dbfff238aa
Devansh Mankar <98455928+devansh-mankar@users.noreply.github.com>
2026-09-19 12:12:10 +0530

    Time: 32 ms (78.84%) | Memory: 53.5 MB (83.31%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -10,7 +10,7 @@ public:
             }
         }
         return true;
-    }   
+    }
     void solve(int index,int n,string& s,vector<string>&temp,vector<vector<string>>&ans)
     {
         if(index==n)
@@ -19,23 +19,20 @@ public:
             return;
         }
 
-        for(int i=index;i<s.size();i++)
+        for(int i=index;i<n;i++)
         {
-            if(valid(s,index,i)){
+            if(valid(s,index,i))
+            {
                 temp.push_back(s.substr(index,i-index+1));
                 solve(i+1,n,s,temp,ans);
                 temp.pop_back();
             }
         }
-
-        
-
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
         vector<string>temp;
         int n=s.size();
-        //string str="";
         solve(0,n,s,temp,ans);
         return ans;
     }
